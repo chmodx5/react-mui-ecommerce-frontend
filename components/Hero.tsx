705bee3b9c60e7545aff7573a25d12e86da7bfb3
chmodx5 <chmodx5@gmail.com>
2021-11-28 09:35:51 +0300

    sidemenu

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import SideMenu from "./Hero/SideMenu";
 import Carousel from "./Hero/Carousel";
 import PromoCard from "./Hero/PromoCard";
-import { Grid } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 
 export default function Hero() {
   return (
-    <Grid container sx={{ height: "70vh", marginTop: "5em" }} spacing={2}>
-      <Grid item style={{ background: "teal" }} xs={12} sm={12} md={3} lg={3}>
+    <React.Fragment>
+      <Box sx={{ height: "600px", marginTop: "4.5em" }}>
         <SideMenu />
+      </Box>
+      <Grid container sx={{ height: "70vh", marginTop: "4.5em" }} spacing={2}>
+        <Grid></Grid>
+        <Grid item md={6}>
+          <Carousel />
+        </Grid>
+        <Grid item md={3}>
+          <PromoCard />
+        </Grid>
       </Grid>
-      <Grid item style={{ background: "red" }} md={6}>
-        <Carousel />
-      </Grid>
-      <Grid item style={{ background: "yellow" }} md={3}>
-        <PromoCard />
-      </Grid>
-    </Grid>
+    </React.Fragment>
   );
 }
--- components/Hero/SideMenu.tsx
+++ components/Hero/SideMenu.tsx
@@ -1,6 +1,375 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Paper,
+  MenuItem,
+  Menu,
+  MenuList,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  ListSubheader,
+} from "@mui/material";
+
+import Check from "@mui/icons-material/Check";
+import { Box } from "@mui/system";
+
+let SideMenuItems = [
+  {
+    icon: "Supermarket",
+    text: "Electronics",
+    sub_items: [
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Computers",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Computers",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Computers",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Computers",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Computers",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    icon: "",
+    text: "Health & Beauty",
+    sub_items: [
+      {
+        text: "soap",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "sanitizer",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    icon: "",
+    text: "Home & Office",
+    sub_items: [
+      {
+        text: "Mobile",
+        sub_items: [
+          {
+            title: "smartphone",
+            link: "/",
+          },
+          {
+            title: "featurephone",
+            link: "/",
+          },
+          {
+            title: "googlephone",
+            link: "/",
+          },
+        ],
+      },
+      {
+        text: "Computers",
+        sub_items: [
+          {
+            title: "laptop",
+            link: "/",
+          },
+          {
+            title: "desktop",
+            link: "/",
+          },
+          {
+            title: "tablet",
+            link: "/",
+          },
+        ],
+      },
+    ],
+  },
+];
 
 export default function SideMenu() {
-  return <Box>this is my box</Box>;
+  const [isShown, setIsShown] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("");
+
+  return (
+    <Paper
+      elevation={4}
+      sx={{
+        height: "600px",
+        width: "auto",
+        display: "inline-block",
+      }}
+    >
+      <Box sx={{ display: "flex", height: "100%" }}>
+        <React.Fragment>
+          <MenuList dense sx={{ width: "250px", height: "100%" }}>
+            {SideMenuItems.map((SideMenuItem, index) => {
+              return (
+                <MenuItem
+                  disableGutters
+                  key={index}
+                  onMouseLeave={() => {
+                    setIsShown(true);
+                    setSelectedItem(SideMenuItem.text);
+                  }}
+                  onMouseEnter={() => {
+                    setIsShown(true);
+                    setSelectedItem(SideMenuItem.text);
+                  }}
+                >
+                  <ListItemIcon>
+                    <Check />
+                  </ListItemIcon>
+                  <ListItemText primary={SideMenuItem.text} />
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+          <Divider orientation="vertical" flexItem />
+          {isShown && (
+            <Box
+              sx={{
+                padding: "0 10px 0 10px",
+                display: "flex",
+                flexWrap: "wrap",
+              }}
+            >
+              {SideMenuItems.map((SideMenuItem, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    {selectedItem === SideMenuItem.text && (
+                      <React.Fragment>
+                        {SideMenuItem.sub_items.map((sub_item, index) => {
+                          return (
+                            <MenuList
+                              dense
+                              sx={{
+                                margin: "4px",
+                              }}
+                              key={index}
+                            >
+                              <ListSubheader>{sub_item.text}</ListSubheader>
+                              {sub_item.sub_items.map((sub_item, index) => {
+                                return (
+                                  <MenuItem key={index}>
+                                    <ListItemText primary={sub_item.title} />
+                                  </MenuItem>
+                                );
+                              })}
+                            </MenuList>
+                          );
+                        })}
+                      </React.Fragment>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </Box>
+          )}
+        </React.Fragment>
+      </Box>
+    </Paper>
+  );
 }
